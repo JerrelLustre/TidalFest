e37a5478f758fd3957f012502cc474182bca26fc
Jerrel <jerrellustre626@gmail.com>
2023-07-27 16:21:48 -0700

    Design FAQ page
    - added h1 and banner

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -1,15 +1,19 @@
 import PageLayout from "../../layouts/PageLayout"
 import MaxWidthLayout from "../../layouts/MaxWidthLayout"
+import H1Container from "../../components/H1Container"
+import SubBanner from "../../components/SubBanner"
+import SectionLayout from "../../layouts/SectionLayout"
 
 export default function Faq() {
   return (
-    <PageLayout
-      h1Text="FAQ"
-      sloganText="Frequently Asked Questions"
-    >
-      <MaxWidthLayout>
-        This is FAQ page.
-      </MaxWidthLayout>
+    <PageLayout>
+      <SectionLayout>
+          <H1Container text="FAQ" />
+          <SubBanner text="Frequently Asked Questions" />
+          <MaxWidthLayout>
+            THIS IS FAQ
+        </MaxWidthLayout>
+      </SectionLayout>
     </PageLayout>
   )
 }
